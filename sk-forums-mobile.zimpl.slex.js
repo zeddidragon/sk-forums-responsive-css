1411926dbe1715872a74628de39599f748991c77
Tony Sæle <zeddy@Tonys-MacBook-Pro.local>
2016-06-25 20:59:16 +0200

    Sleipnir extension

---
+++ sk-forums-mobile.zimpl.slex.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name  Spiral knights forums responsive styles
+// @author  Zeddy
+// @description Make SK forums less horrible on phones
+// @include http://forums.spiralknights.com/en/
+// @version 1.0
+// @require api
+// ==/UserScript==
+
+//REQUIRE ./insert-viewport-tag.js REQUIRE//
+
+var css = "//REQUIRE ./sk-forums-mobile.css REQUIRE//"
