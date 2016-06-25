8591c3c0cd951bdbacc78baf74272d39aab7beb1
Tony Sæle <zeddy@Tonys-MacBook-Pro.local>
2016-06-25 21:16:34 +0200

    derp

--- sk-forums-mobile.zimpl.slex.js
+++ sk-forums-mobile.zimpl.slex.js
@@ -2,11 +2,12 @@
 // @name  Spiral knights forums responsive styles
 // @author  Zeddy
 // @description Make SK forums less horrible on phones
-// @include http://forums.spiralknights.com/en/
-// @version 1.0
-// @require api
+// @include http://forums.spiralknights.com/*
+// @version 1.0.1
 // ==/UserScript==
 
+alert('before everything')
 //REQUIRE ./insert-viewport-tag.js REQUIRE//
 
 var css = "//REQUIRE ./sk-forums-mobile.css REQUIRE//"
+alert('after everything')
