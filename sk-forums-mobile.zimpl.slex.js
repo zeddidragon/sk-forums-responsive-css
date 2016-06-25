533983470361b72a257994c9adec0f3589fe18d8
Tony Sæle <zeddy@Tonys-MacBook-Pro.local>
2016-06-25 22:20:37 +0200

    Using backticks helps

--- sk-forums-mobile.zimpl.slex.js
+++ sk-forums-mobile.zimpl.slex.js
@@ -3,12 +3,12 @@
 // @author  Zeddy
 // @description Make SK forums less horrible on phones
 // @include http://forums.spiralknights.com/*
-// @version 1.0.1
+// @version 1.0.2
 // @require jquery
 // ==/UserScript==
 
 $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">')
-$('head').append('
+$('head').append(`
 <style>
 //REQUIRE ./sk-forums-mobile.css REQUIRE//
 </style>
