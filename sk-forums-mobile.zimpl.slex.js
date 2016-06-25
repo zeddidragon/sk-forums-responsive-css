30998e9df7abb6ea5beee47501a92d24393c2f63
Tony Sæle <zeddy@Tonys-MacBook-Pro.local>
2016-06-25 22:36:54 +0200

    checking if error

--- sk-forums-mobile.zimpl.slex.js
+++ sk-forums-mobile.zimpl.slex.js
@@ -7,10 +7,13 @@
 // @require jquery
 // ==/UserScript==
 
+try {
 $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">')
 $('head').append(`
 <style>
 //REQUIRE ./sk-forums-mobile.css REQUIRE//
 </style>
 `)
-
+} catch(e) {
+  alert("This went wrong: " + e.stack)
+}
