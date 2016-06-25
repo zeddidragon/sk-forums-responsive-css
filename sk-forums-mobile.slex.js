cb825dbb8200e4ec8e970083270934f99c3dfe27
Tony Sæle <zeddy@Tonys-MacBook-Pro.local>
2016-06-25 22:47:09 +0200

    just the meta tag again?

--- sk-forums-mobile.slex.js
+++ sk-forums-mobile.slex.js
@@ -3,17 +3,9 @@
 // @author  Zeddy
 // @description Make SK forums less horrible on phones
 // @include http://forums.spiralknights.com/*
-// @version 1.0.2
+// @version 1.0.3
 // @require jquery
 // ==/UserScript==
-alert('script')
-
-try {
 alert('trying to append viewport')
 $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">')
 alert('viewport appended')
-$('head').append('<style style="display: block"> honk: stonk; </style>')
-alert('stylesheet also appended')
-} catch(e) {
-  alert("This went wrong: " + e.stack)
-}
